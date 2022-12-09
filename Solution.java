a9daf29dc772231238de41d8d2a5b4fa53b26b04
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-12-09 20:28:24 +0100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,13 +1,18 @@
 
 public class Solution {
 
+    private static final char CUSTOMER_VISITS = 'Y';
+    private static final char NO_CUSTOMER_VISITS = 'N';
+    private static final int PROFIT = 1;
+    private static final int PENALTY = -1;
+
     public int bestClosingTime(String customers) {
         int currentProfit = 0;
         int maxProfit = 0;
         int earliestClosingHourWithMinLoss = 0;
 
         for (int hour = 0; hour < customers.length(); ++hour) {
-            currentProfit += customers.charAt(hour) == 'Y' ? 1 : -1;
+            currentProfit += (customers.charAt(hour) == CUSTOMER_VISITS) ? PROFIT : PENALTY;
             if (currentProfit > maxProfit) {
                 maxProfit = currentProfit;
                 earliestClosingHourWithMinLoss = hour + 1;
